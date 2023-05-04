661d7330c6116394587eaa330def0be6be366f98
dupe17 <114872307+dupe17@users.noreply.github.com>
2023-05-04 11:57:59 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -83,7 +83,9 @@ $(function() { // Same as document.addEventListener("DOMContentLoaded"...
         showLoading("#main-content");
         $ajaxUtils.sendGetRequest(
             allCategoriesUrl,
-            buildAndShowHomeHTML, // ***** <---- TODO: STEP 1: Substitute [...] ******
+            function(request){
+            buildAndShowHomeHTML(request);
+            }, // ***** <---- TODO: STEP 1: Substitute [...] ******
             true); // Explicitely setting the flag to get JSON from server processed into an object literal
     });
     // *** finish **
@@ -100,7 +102,7 @@ $(function() { // Same as document.addEventListener("DOMContentLoaded"...
                 // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
                 // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
                 // variable's name implies it expects.
-                // var chosenCategoryShortName = ....
+                // var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
 
 
                 // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
@@ -115,9 +117,9 @@ $(function() { // Same as document.addEventListener("DOMContentLoaded"...
                 // it into the home html snippet.
                 //
                 // var homeHtmlToInsertIntoMainPage = ....
+var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", "'" + chosenCategoryShortName + "'");
 
-
-                // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
+// TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
                 // Use the existing insertHtml function for that purpose. Look through this code for an example
                 // of how to do that.
                 // ....
